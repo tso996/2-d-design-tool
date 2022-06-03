b6f0a6e793a40f4c53f33237de31760a7f1e34a5
Joe <51638822+tso996@users.noreply.github.com>
2022-06-03 08:49:00 +0100

    added user interaction functions to the main class

--- main.cpp
+++ main.cpp
@@ -13,15 +13,29 @@
 
 #include "olcPixelGameEngine.h"
 
-class Polymorphism : public olc::PixelGameEngine{
+class DesignTool : public olc::PixelGameEngine{
     public:
-        Polymorphism(){
+        DesignTool(){//constructor
             sAppName = "Polymorphism";
+        }
+
+        bool OnUserCreate() override{//overrides mean that the compiler will throw an error if the signature of the function changes from the base class function
+            return true;
+        }
+
+        bool OnUserUpdate(float fElapsedTime) override{
+            olc::vf2d vMouse = { (float)GetMouseX(), (float)GetMouseY()};
+            return true;
         }
 };
 
 
 
 int main(){
+
+    DesignTool demo;
+    if(demo.Construct(1600, 960, 1, 1)){//pge function since it is the parent class of DesignTool
+        demo.Start();
+    }
     return 0;
 }
